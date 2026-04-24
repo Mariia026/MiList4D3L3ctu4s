1216aa63581afb1061d73e3f7913a2bda5f58201
Mariia026 <74515206+Mariia026@users.noreply.github.com>
2026-04-23 23:15:57 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,22 @@
-const CACHE="v1";
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/","/index.html","/assets/js/app.js","/assets/css/styles.css"])));
+const CACHE = "biblioteca-v1";
+
+const urlsToCache = [
+  "/MiList4D3L3ctu4s/",
+  "/MiList4D3L3ctu4s/index.html",
+  "/MiList4D3L3ctu4s/assets/js/app.js",
+  "/MiList4D3L3ctu4s/assets/css/styles.css"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
