f12165015dcb4e41031bd6e05ee0a870ee163be2
Mariia026 <74515206+Mariia026@users.noreply.github.com>
2026-05-08 04:19:19 -0300

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,22 @@
-const CACHE = "biblioteca-final-v1";
+const CACHE = "mi-biblioteca-v3";
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE).then(cache => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./assets/js/app.js",
-        "./assets/css/styles.css"
+        "/MiList4D3L3ctu4s/",
+        "/MiList4D3L3ctu4s/index.html",
+        "/MiList4D3L3ctu4s/assets/js/app.js",
+        "/MiList4D3L3ctu4s/assets/css/styles.css"
       ]);
     })
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).catch(() => caches.match("./index.html"));
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
     })
   );
 });
